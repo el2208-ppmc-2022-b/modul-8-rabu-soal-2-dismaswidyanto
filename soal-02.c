b0044cb84fd1588f75b0ad17f758d66ffb06ca50
dismaswidyanto <63232560+dismaswidyanto@users.noreply.github.com>
2022-04-11 21:12:35 +0700

    Update soal-02.c

--- soal-02.c
+++ soal-02.c
@@ -12,6 +12,25 @@
 #include<string.h>
 #include<stdlib.h>
 
+/**
+ * @brief Fungsi untuk memroses matriks
+ * 
+ * @param mat matriks yang ditinjau
+ * @param col indeks untuk kolom yang sedang ditinjau
+ * @param row indeks untuk baris yang sedang ditinjau
+ * @return (int) dikembalikan dengan nilai integer
+ */
+int process(int mat[10][10], int col, int row);
+
+/**
+ * @brief Fungsi untuk menentukan input mana yang lebih besar
+ * 
+ * @param a input 1
+ * @param b input 2
+ * @return (int) bilangan yang lebih besar
+ */
+int max(int a, int b);
+
 int main() {
 	// Inisialisasi stream open
 	FILE *fp;
@@ -30,8 +49,8 @@ int main() {
 
 	// Buka file
 	printf("Masukkan nama file: ");
-	scanf("%s", ...);
-	fp = fopen(..., "r");
+	scanf("%s", namafile);
+	fp = fopen(namafile, "r");
 
 	// Ekstraksi informasi dari file
 	x = 0; z = 0;
@@ -56,16 +75,192 @@ int main() {
 		x++;
 	}
 
-	// Di tahap ini, matriks sudah selesai dibuat
+	int res = process(mat,0,0);
+	printf("Makanan terbanyak: %d",res);
+}
 
-	/**
-	 * 
-	 * Kamu bisa mengisi bagian ini untuk algoritma pencarian jalur makanan terbanyak
-	 * Semangat modul terakhirnya :D Mudah2an templatenya membantu (y)
-	 * 
-	 * */
+int process(int mat[10][10], int col, int row) {
+	// Inisialisasi hasil komputasi untuk 4 cabang
+	int res1 = 0, res2 = 0, res3 = 0, res4 = 0;
+	// Kurangi makanan pada node yang sedang dipijak
+	mat[row][col] -= 1;
 
-	printf("Makanan terbanyak: %d",...);
+	// Evaluasi kiri dan kanan node yang dipijak
+	if (row == 0 && col == 0) {
+		if (mat[row+1][col] <= 0) {
+			// do nothing 
+		}
+		else {
+			res1 = process(mat, col, row+1);
+		}
+		if (mat[row][col+1] <= 0) {
+			// do nothing
+		}
+		else {
+			res2 = process(mat, col+1, row);
+		}
+	}
+	else if (row == 9 && col == 0) {
+		if (mat[row][col+1] <= 0) {
+			// do nothing
+		}
+		else {
+			res1 = process(mat, col+1, row);
+		}
+		if (mat[row-1][col] <= 0) {
+			// do nothing
+		}
+		else {
+			res2 = process(mat, col, row-1);
+		}
+	}
+	else if (row == 0 && col == 9) {
+		if (mat[row][col-1] <= 0) {
+			// do nothing
+		}
+		else {
+			res1 = process(mat, col-1, row);
+		}
+		if (mat[row+1][col] <= 0) {
+			// do nothing
+		}
+		else {
+			res2 = process(mat, col, row+1);
+		}
+	}
+	else if (row == 9 && col == 9) {
+		if (mat[row][col-1] <= 0) {
+			// do nothing
+		}
+		else {
+			res1 = process(mat, col-1, row);
+		}
+		if (mat[row-1][col] <= 0) {
+			// do nothing
+		}
+		else {
+			res2 = process(mat, col, row-1);
+		}
+	}
+	else if (row == 0 && col != 0 && col != 9) {
+		if (mat[row][col-1] <= 0) {
+			// do nothing
+		}
+		else {
+			res1 = process(mat, col-1, row);
+		}
+		if (mat[row][col+1] <= 0) {
+			// do nothing
+		}
+		else {
+			res2 = process(mat, col+1, row);
+		}
+		if (mat[row+1][col] <= 0) {
+			// do nothing
+		}
+		else {
+			res3 = process(mat, col, row+1);
+		}
+	}
+	else if (row == 9 && col != 0 && col != 9) {
+		if (mat[row][col-1] <= 0) {
+			// do nothing
+		}
+		else {
+			res1 = process(mat, col-1, row);
+		}
+		if (mat[row][col+1] <= 0) {
+			// do nothing
+		}
+		else {
+			res2 = process(mat, col+1, row);
+		}
+		if (mat[row-1][col] <= 0) {
+			// do nothing
+		}
+		else {
+			res3 = process(mat, col, row-1);
+		}
+	}
+	else if (col == 0 && row != 0 && row != 9) {
+		if (mat[row-1][col] <= 0) {
+			// do nothing
+		}
+		else {
+			res1 = process(mat, col, row-1);
+		}
+		if (mat[row+1][col] <= 0) {
+			// do nothing
+		}
+		else {
+			res2 = process(mat, col, row+1);
+		}
+		if (mat[row][col+1] <= 0) {
+			// do nothing
+		}
+		else {
+			res3 = process(mat, col+1, row);
+		}
+	}
+	else if (col == 9 && row != 0 && row != 9) {
+		if (mat[row-1][col] <= 0) {
+			// do nothing
+		}
+		else {
+			res1 = process(mat, col, row-1);
+		}
+		if (mat[row+1][col] <= 0) {
+			// do nothing
+		}
+		else {
+			res2 = process(mat, col, row+1);
+		}
+		if (mat[row][col-1] <= 0) {
+			// do nothing
+		}
+		else {
+			res3 = process(mat, col-1, row);
+		}
+	}
+	else {
+		if (mat[row-1][col] <= 0) {
+			// do nothing
+		}
+		else {
+			res1 = process(mat, col, row-1);
+		}
+		if (mat[row+1][col] <= 0) {
+			// do nothing
+		}
+		else {
+			res2 = process(mat, col, row+1);
+		}
+		if (mat[row][col-1] <= 0) {
+			// do nothing
+		}
+		else {
+			res3 = process(mat, col-1, row);
+		}
+		if (mat[row][col+1] <= 0) {
+			// do nothing
+		}
+		else {
+			res4 = process(mat, col+1, row);
+		}
+	}
 
-	return 0;
+	// Kembalikan nilai makanan yang sudah dipijak untuk rekursi cabang lain
+	mat[row][col] += 1;
+
+	// Kembalikan nilai tertinggi
+	return(1+max(max(res1, res2), max(res3, res4)));
+}
+
+int max(int a, int b) {
+	if (a>b) {
+		return a;
+	}
+	else {
+		return b;
+	}
 }
